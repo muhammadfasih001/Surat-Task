b3ea9e42f3c911d23b7caf712466c8666e295d94
muhammadfasih <muhammadfasih006@gmail.com>
2024-08-14 02:29:05 +0500

    task upload

--- lib/services/surah_api_service.dart
+++ lib/services/surah_api_service.dart
@@ -1,34 +1,3 @@
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-// import '../models/surah_model.dart';
-
-// class ApiService {
-//   final String _baseUrl = 'https://api.alquran.cloud/v1';
-
-//   Future<SurahModel> fetchSurahs() async {
-//     final response = await http.get(Uri.parse('$_baseUrl/quran/ur.jhaladhry'));
-//     if (response.statusCode == 200) {
-//       return SurahModel.fromJson(json.decode(response.body));
-//     } else {
-//       throw Exception('Failed to load surahs');
-//     }
-//   }
-
-//   Future<Surahs> fetchAyahs(int surahNumber) async {
-//     final response = await http.get(Uri.parse('$_baseUrl/surah/$surahNumber'));
-//     if (response.statusCode == 200) {
-//       final data = json.decode(response.body);
-
-//       final surahModel = SurahModel.fromJson(data);
-//       return surahModel.data?.surahs
-//               ?.firstWhere((s) => s.number == surahNumber) ??
-//           Surahs();
-//     } else {
-//       throw Exception('Failed to load ayahs');
-//     }
-//   }
-// }
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/surah_model.dart';
